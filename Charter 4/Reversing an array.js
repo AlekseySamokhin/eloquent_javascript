0005bc3e22527e6a2c762f0f3df85e7d8af346dc
AlekseySamokhin <lel.dude01@gmail.com>
2022-09-07 10:13:27 +0300

    added solutions tasks 4 charter

---
+++ Charter 4/Reversing an array.js
@@ -0,0 +1,62 @@
+// 1
+function reverseArray(array) {
+    let newArray = [];
+
+    for(let i = array.length - 1; i >= 0; i--) {
+        newArray.push(array[i]);
+    }
+    
+    return newArray;
+}
+
+console.log(reverseArray(["A", "B", "C"]));
+
+function reverseArrayInPlace(arrayValue) {
+    const arrayLength = arrayValue.length;
+    
+    for(let i = 0; i < arrayLength; i++) {
+        arrayValue[arrayLength + i] = arrayValue[arrayLength - i - 1];
+    }
+
+    return arrayValue.slice(0, arrayLength);
+}
+
+// 2 
+function reverseArray(array) {
+    return array.reduce(function(a, b) {
+        return [b].concat(a);
+    }, []);
+}
+
+console.log(reverseArray(["A", "B", "C"]));
+
+function reverseArrayInPlace(arrayValue) {
+    const arrayLength = arrayValue.length;
+    for(let i = 0; i < Math.floor(arrayLength / 2); i++) {
+        const old = arrayValue[i];
+        arrayValue[i] = arrayValue[arrayLength - 1 - i];
+        arrayValue[arrayLength - 1 - i] = old;
+    }
+
+    return arrayValue;
+}
+
+// 3 
+function reverseArrayInPlace(arrayValue) {
+    let newArray = [];
+    const arrayLength = arrayValue.length;
+    
+    for(let i = arrayLength - 1; i >= 0; i--) {
+        newArray.unshift(arrayValue.pop());
+    }
+
+    for(let i = arrayLength - 1; i >= 0; i--) {
+        arrayValue[i] = newArray.shift();
+    }
+
+    return arrayValue;
+}
+
+let arrayValue = [1, 2, 3, 4, 5];
+reverseArrayInPlace(arrayValue);
+console.log(arrayValue);
